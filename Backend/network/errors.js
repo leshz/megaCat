9b4d737c0264847fbcc13949ebe24646cd623ec9
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-05-26 14:03:52 -0500

    Fix error middleware

--- Backend/network/errors.js
+++ Backend/network/errors.js
@@ -1,10 +1,17 @@
 const response = require('./response')
 
 function errors (err, req, res, next) {
-  const {
-    message = 'Internal error',
-    statusCode = 500
-  } = err
+  let message = 'Internal error'
+  let statusCode = 500
+
+  if (err.output && err.output.payload) {
+    message = err.output.payload.message
+    statusCode = err.output.payload.statusCode
+  } else if (err.message || err.statusCode) {
+    message = err.message || message
+    statusCode = err.statusCode || statusCode
+  }
+
   response.error(req, res, message, statusCode)
 }
 
