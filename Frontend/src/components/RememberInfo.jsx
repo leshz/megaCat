3c5e5d19d94c6a50fc88394d8be73ae2cc430cf8
Fer Torres <fertorresmx@gmail.com>
2020-05-27 22:02:18 -0500

    Fix problems from eslint

--- Frontend/src/components/RememberInfo.jsx
+++ Frontend/src/components/RememberInfo.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/static/logo.png'
-import '../assets/styles/components/RememberInfo.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/static/logo.png';
+import '../assets/styles/components/RememberInfo.scss';
 
 const RememberInfo = () => {
   return (
-    <section className="RememberInfo">
-      <Link to="/">
-        <figure className="Logo">
-          <img src={logo} alt="Logo de Nextep" />
+    <section className='RememberInfo'>
+      <Link to='/'>
+        <figure className='Logo'>
+          <img src={logo} alt='Logo de Nextep' />
         </figure>
       </Link>
-      <div className="RememberInfo__container">
+      <div className='RememberInfo__container'>
         <h1>
           Digite su usuario, se enviará un link a su correo asociado para
           reestablecer la contraseña
         </h1>
-        <form className="RememberInfo__container--form">
-          <div className="RememberInfo__container--form--options">
+        <form className='RememberInfo__container--form'>
+          <div className='RememberInfo__container--form--options'>
             <input
-              name="email"
-              className="input"
-              type="text"
-              placeholder="Usuario"
+              name='email'
+              className='input'
+              type='text'
+              placeholder='Usuario'
             />
             <span>
-              <i className="fas fa-user"></i>
+              <i className='fas fa-user' />
             </span>
           </div>
-          <button type="submit">Enviar correo</button>
+          <button type='submit'>Enviar correo</button>
         </form>
       </div>
     </section>
   );
-}
+};
 
-export default RememberInfo
+export default RememberInfo;
