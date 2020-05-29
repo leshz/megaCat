f3dbbeabf04dd07bbe3c80cf30681ee4e7227738
Fer Torres <fertorresmx@gmail.com>
2020-05-28 20:35:54 -0500

    Add menu to header profile

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import iconType from '../assets/static/iconotipo.png';
 import logo from '../assets/static/logotipo.png';
@@ -8,18 +9,50 @@ import '../assets/styles/components/Header.scss';
 const Header = () => (
   <header className='header'>
     <div className='main-container'>
+
       <figure className='imagotipo'>
-        <img className='iconotipo' src={iconType} alt='Iconotipo de Netxtep' />
-        <img className='logotipo' src={logo} alt='logotipo' />
+        <Link to='/'>
+          <img
+            className='iconotipo'
+            src={iconType}
+            alt='Iconotipo de Netxtep'
+          />
+          <img
+            className='logotipo'
+            src={logo}
+            alt='logotipo'
+          />
+        </Link>
       </figure>
+
       <span className='tittle-container'>
         <h1 className='tittle'>Mis Resultados de Laboratorio</h1>
       </span>
-      <span className='user-container'>
-        <a href='usersesion'>
-          <img className='user-icon' src={userIcon} alt='User' />
-        </a>
-      </span>
+
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          <img
+            className='user-icon'
+            src={userIcon}
+            alt='User Profile'
+          />
+          <p>Perfil</p>
+        </div>
+
+        <ul>
+          <li>
+            <Link to='/login'>
+              Cuenta
+            </Link>
+          </li>
+          <li>
+            <Link to='/login'>
+              Iniciar Sesión
+            </Link>
+          </li>
+        </ul>
+      </div>
+
       <span>
         <p className='patient-indication'>Panel del Paciente</p>
       </span>
