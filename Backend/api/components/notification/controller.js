0bf7c482f218999afb08a3ba0effe27129ab2040
Jeff Barragan <leshz@me.com>
2020-05-30 21:19:44 -0500

    Add Firebase entrypoint token

--- Backend/api/components/notification/controller.js
+++ Backend/api/components/notification/controller.js
@@ -1,7 +1,11 @@
 const config = require('../../../config/config')
 const admin = require('firebase-admin')
+// const boom = require('@hapi/boom')
+
 const FB_APP_NAME = config.fbase.name
 const FB_CERT = config.fbase.cert
+
+const TABLE = 'firebaseNotificationTokens'
 
 const appFirebase = admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(FB_CERT)),
@@ -26,8 +30,24 @@ module.exports = (store) => {
         .catch((err) => reject(err))
     })
   }
+  function setTokenToUser (profile) {
+    return new Promise((resolve, reject) => {
+      // if (profile === null) { reject(boom.internal) }
+      // console.log(profile)
+      console.log('INSERTTTT')
+
+      store.insert(TABLE, { userId: '3b09f3f7-5f00-4643-9dfc-6a42a6eb0cb0', token: '3efg' })
+        .then(e => {
+          console.log(e)
+          resolve(e)
+        }).catch(e => {
+          console.log(e, 'ERROR')
+        })
+    })
+  }
 
   return {
-    sendMessageMulticast
+    sendMessageMulticast,
+    setTokenToUser
   }
 }
--- Backend/api/components/notification/network.js
+++ Backend/api/components/notification/network.js
@@ -2,10 +2,12 @@ const Controller = require('./index')
 
 const express = require('express')
 const response = require('../../../network/response')
-
+// const passport = require('passport')
 const router = express.Router()
 
 router.get('/', testNotification)
+router.post('/setToken', addTokentoUser)
+// passport.authenticate('basic', { session: true }),
 
 function testNotification (req, res) {
   return Controller.sendMessageMulticast(req.body)
@@ -14,6 +16,16 @@ function testNotification (req, res) {
       response.success(req, res, data, 200)
     })
     .catch((err) => {
+      response.error(req, res, err, 401)
+    })
+}
+
+function addTokentoUser (req, res) {
+  return Controller.setTokenToUser((req && req.user) || null)
+    .then(data => {
+      response.success(req, res, data, 200)
+    })
+    .catch(err => {
       response.error(req, res, err, 401)
     })
 }
