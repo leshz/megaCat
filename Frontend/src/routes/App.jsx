77f596cf06c95e46bf299b62835f654e61ee905f
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-05-31 20:35:19 -0500

    Auth advance

--- Frontend/src/routes/App.jsx
+++ Frontend/src/routes/App.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from '../components/Layout';
-import Login from '../containers/HomeLogin';
-import RememberInfo from '../components/RememberInfo';
-import Bacteriologist from '../containers/Bacteriologist';
-import AddExamsResults from '../containers/AddExamsResults';
-import Doctor from '../containers/Doctor';
-import PatientInfo from '../containers/PatientInfo';
-import PersonalInfo from '../components/PersonalInfo';
-import MedicalHistory from '../components/MedicalHistory';
-import SheduleExams from '../components/SheduleExams';
-import Administrator from '../containers/Administrator';
-import Patient from '../containers/Patient';
-import NotFound from '../containers/NotFound';
-import '../assets/styles/App.scss';
-import AddUser from '../containers/AddUser';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux"
+import Layout from "../components/Layout";
+import Login from "../containers/HomeLogin";
+import RememberInfo from "../components/RememberInfo";
+import Bacteriologist from "../containers/Bacteriologist";
+import AddExamsResults from "../containers/AddExamsResults";
+import Doctor from "../containers/Doctor";
+import PatientInfo from "../containers/PatientInfo";
+import PersonalInfo from "../components/PersonalInfo";
+import MedicalHistory from "../components/MedicalHistory";
+import SheduleExams from "../components/SheduleExams";
+import Administrator from "../containers/Administrator";
+import Patient from "../containers/Patient";
+import NotFound from "../containers/NotFound";
+import "../assets/styles/App.scss";
+import AddUser from "../containers/AddUser";
 
-const App = () => {
+const App = ({ user }) => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/remember" component={RememberInfo} />
+        {/* {user && user.roles && user.roles.length > 0 && ( */}
         <Layout>
-          <Route exact path="/bacteriologist" component={Bacteriologist} />
-          <Route exact path="/addExamResults" component={AddExamsResults} />
+          { user && user.roles && user.roles.find(role => role.name === "bacteriologist") > 0 && (
+            <>
+              <Route exact path="/bacteriologist" component={Bacteriologist} />
+              <Route exact path="/addExamResults" component={AddExamsResults} />
+            </>
+          )}
           <Route exact path="/doctor" component={Doctor} />
           <Route
             path="/(personalInfo|medicalHistory|sheduleExams)/"
@@ -37,10 +43,15 @@ const App = () => {
           <Route exact path="/patient" component={Patient} />
           <Route exact path="/addUser" component={AddUser} />
         </Layout>
+        {/* )} */}
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
 };
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(App);
