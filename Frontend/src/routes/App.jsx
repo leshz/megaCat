de2f29c07e32927167100130794ea4c415d71d84
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-05-31 21:25:29 -0500

    Advance on routes authorization and authentication

--- Frontend/src/routes/App.jsx
+++ Frontend/src/routes/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 import Layout from "../components/Layout";
 import Login from "../containers/HomeLogin";
 import RememberInfo from "../components/RememberInfo";
@@ -23,27 +23,45 @@ const App = ({ user }) => {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/remember" component={RememberInfo} />
-        {/* {user && user.roles && user.roles.length > 0 && ( */}
         <Layout>
-          { user && user.roles && user.roles.find(role => role.name === "bacteriologist") > 0 && (
+          {user &&
+            user.roles &&
+            user.roles.find((role) => role.name === "bacteriologist") && (
             <>
               <Route exact path="/bacteriologist" component={Bacteriologist} />
               <Route exact path="/addExamResults" component={AddExamsResults} />
             </>
           )}
-          <Route exact path="/doctor" component={Doctor} />
-          <Route
-            path="/(personalInfo|medicalHistory|sheduleExams)/"
-            component={PatientInfo}
-          />
-          <Route exact path="/personalInfo" component={PersonalInfo} />
-          <Route exact path="/medicalHistory" component={MedicalHistory} />
-          <Route exact path="/sheduleExams" component={SheduleExams} />
-          <Route exact path="/administrator" component={Administrator} />
-          <Route exact path="/patient" component={Patient} />
-          <Route exact path="/addUser" component={AddUser} />
+          {user &&
+            user.roles &&
+            user.roles.find((role) => role.name === "doctor") && (
+            <>
+              <Route exact path="/doctor" component={Doctor} />
+              <Route
+                path="/(personalInfo|medicalHistory|sheduleExams)/"
+                component={PatientInfo}
+              />
+              <Route exact path="/personalInfo" component={PersonalInfo} />
+              <Route exact path="/medicalHistory" component={MedicalHistory} />
+              <Route exact path="/sheduleExams" component={SheduleExams} />
+            </>
+          )}
+          {user &&
+            user.roles &&
+            user.roles.find((role) => role.name === "administrator") && (
+            <>
+              <Route exact path="/administrator" component={Administrator} />
+              <Route exact path="/addUser" component={AddUser} />
+            </>
+          )}
+          {user &&
+            user.roles &&
+            user.roles.find((role) => role.name === "patient") && (
+            <>
+              <Route exact path="/patient" component={Patient} />
+            </>
+          )}
         </Layout>
-        {/* )} */}
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
